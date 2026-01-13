efa8c12d5d0f3e70469265f6ce45a717379dde2a
TRENT-13 <tatokaxniashvili7@gmail.com>
2026-01-13 23:27:01 +0400

    refined code

--- utils/LLM_call.py
+++ utils/LLM_call.py
@@ -1,19 +1,19 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-def _load_llm_OPEN(model_name : str, temperature: float) -> ChatOpenAI:
-    load_dotenv()
+
+# Load once at module level
+load_dotenv()
+
+def _load_llm_OPEN(model_name: str, temperature: float) -> ChatOpenAI:
     api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
         raise ValueError("Missing OPENAI_API_KEY in environment. Very bad naughty naughty!")
-    model_name = model_name
+    
     return ChatOpenAI(model=model_name, api_key=api_key, temperature=temperature)
 
 
-
 def _load_llm_deepseek(model_name: str, temperature: float) -> ChatOpenAI:
-    load_dotenv()
-
     api_key = os.getenv("DEEPSEEK_API_KEY")
     if not api_key:
         raise ValueError("Missing DEEPSEEK_API_KEY in environment")
